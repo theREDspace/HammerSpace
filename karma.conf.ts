bc25d30ba5e789e3e51cc716bc970c55c6e8d37a
Luke McKeen <luke.mckeen@redspace.com>
2017-05-30 15:09:07 -0300

    fixing karma config to allow for all ts files and not just spec.ts files, also added the coverage folder to the gitignore

--- karma.conf.ts
+++ karma.conf.ts
@@ -15,7 +15,7 @@ module.exports = (config: any) => {
 
     // list of files / patterns to load in the browser
     files: [
-      './src/**/*.spec.ts'
+      './src/**/*.ts'
     ],
 
 
@@ -28,7 +28,7 @@ module.exports = (config: any) => {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      './src/**/*.spec.ts': ['karma-typescript']
+      './src/**/*.ts': ['karma-typescript']
     },
 
 
