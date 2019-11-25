282154f5057d8d751665555bed5c2cf27be4ba6a
Luke Mckeen <luke.mckeen@redspace.com>
2019-11-24 23:10:06 -0400

    adding docs

--- src/client/client.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface ClientEvents {
-  [key: string]: (data?: {}) => void | undefined;
-}
-
-export interface ClientType {
-  [key: string]: (name: {} | string, data?: string, isRebound?: boolean) => void
-}
--- src/rebound/rebound.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface ReboundEvent {
-  id?: string,
-  event: string,
-  value?: string | number | object
-}
-
-export interface ReboundType {
-  [key: string]: (name: {} | string, data?: string) => void;
-}
